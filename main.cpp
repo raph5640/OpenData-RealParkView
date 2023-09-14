73c83bdf09572685a0788439b0308c85c13393a9
Raphael De Oliveira <Raphaeldeoliveira5640@gmail.com>
2023-09-14 11:50:56 +0200

    Amélioration affichage nom des parkings sur plusieurs ligne quand pourcentage est <90

--- main.cpp
+++ main.cpp
@@ -22,35 +22,51 @@ void download_json() {
     }
 }
 
+vector<string> split_into_lines(const string& str) {
+    vector<string> result;
+    stringstream ss(str);
+    string word;
 
+    while (getline(ss, word, ' ')) { //decoupe la chaine a chaque espace
+        result.push_back(word);
+    }
+
+    return result;
+}
 // Fonction pour créer un histogramme
 void create_histogram(const string& filename, const vector<string>& noms, const vector<int>& dispo, const vector<int>& max) {
-    const int image_width = 1200;
-    const int image_height = 400;
+    const int image_width = 1600;
+    const int image_height = 600;
     const int bar_width = image_width / noms.size();
+    const int MAX_BAR_HEIGHT = image_height - 20;
 
     gdImagePtr im = gdImageCreateTrueColor(image_width, image_height);
     int white = gdImageColorAllocate(im, 255, 255, 255);
     gdImageFill(im, 0, 0, white);
 
-    int black = gdImageColorAllocate(im, 0, 0, 0);  // Couleur pour le texte et les axes
+    int black = gdImageColorAllocate(im, 0, 0, 0);  // Couleur pour le texte
 
     //Dessin des barres de l'histogramme
     for (size_t i = 0; i < dispo.size(); i++) {
         int pourcentage = (100 * dispo[i]) / max[i];
         int bar_height = (image_height * pourcentage) / 100;
+        bar_height = min(bar_height, MAX_BAR_HEIGHT);
         int color = gdImageColorAllocate(im, rand() % 255, rand() % 255, rand() % 255);
 
         gdImageFilledRectangle(im, i * bar_width, image_height - bar_height, (i+1) * bar_width, image_height, color);
 
-        //Ajout du texte pour le nom du parking au-dessus de chaque barre
         string label = noms[i] + " (" + to_string(pourcentage) + "%)";
-        gdImageString(im, gdFontGetSmall(), i * bar_width + 5, image_height - bar_height - 15, (unsigned char*)label.c_str(), black);
+        if (pourcentage < 90) {     //Si le pourcentage du parking est inferieur a 90% on écrit son nom sur plusieurs ligne pour éviter les cheuvauchements du texte sur les autres barres suivantes
+            auto lines = split_into_lines(label);
+            int offsetY = (lines.size() - 1) * 12;
+            for (const auto& line : lines) {
+                gdImageString(im, gdFontGetSmall(), i * bar_width + 5, image_height - bar_height - 15 - offsetY, (unsigned char*)line.c_str(), black);
+                offsetY -= 12;  //décrémente la valeur pour aller vers le haut
+            }
+        } else {  //sinon si le pourcentage est tres haut (>90) on considerer que si l'on affiche le nom entier du parking sur une ligne ca ne pose pas de probleme d'affichage et de lisibilité
+            gdImageString(im, gdFontGetSmall(), i * bar_width + 5, image_height - bar_height - 15, (unsigned char*)label.c_str(), black);
+        }
     }
-
-    //Dessin des axes
-    gdImageLine(im, 0, image_height - 1, image_width, image_height - 1, black);  //Axe des x
-    gdImageLine(im, 0, 0, 0, image_height, black);  //Axe des y
 
     FILE* out = fopen(filename.c_str(), "wb");
     gdImagePng(im, out);
